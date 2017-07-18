fee58e42dedcf139df2663b245777063adc87b5c
MagicPantalones <magicpantalones@gmail.com>
2017-07-19 01:33:22 +0200

    experiMENTAL

---
+++ app/src/androidTest/java/com/example/android/colorlocationtester/colorCoordinatesTest.java
@@ -0,0 +1,31 @@
+package com.example.android.colorlocationtester;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Erik on 18.07.2017.
+ */
+public class colorCoordinatesTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void onDraw() throws Exception {
+
+    }
+
+    @Test
+    public void analyzeColors() throws Exception {
+
+    }
+
+}
